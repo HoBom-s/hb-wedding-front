d7faae6d3449e4fd1f43cea4b50d207ee4a261db
foxmon <jjockrod@naver.com>
2024-04-22 23:41:13 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  webpack: (webpackConfig) => {
+    const prod = process.env.NODE_ENV === "production";
+    const config = {
+      ...webpackConfig,
+      mode: prod ? "production" : "development",
+    };
+
+    if (prod) {
+      config.devtool = "hidden-source-map";
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
